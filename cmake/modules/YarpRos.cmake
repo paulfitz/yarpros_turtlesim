66562fe5ed6b82be1ae7941988779ee92f4a4815
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-06-07 16:43:41 +0200

    update 'yarpros type' -> 'yarpidl_rosmsg'

--- cmake/modules/YarpRos.cmake
+++ cmake/modules/YarpRos.cmake
@@ -2,7 +2,7 @@ macro(YARPROS_ADD_TYPE _type)
 
     add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/${_type}.h
                        COMMAND cmake ARGS -E copy_if_different ${CMAKE_SOURCE_DIR}/${_type}.msg ${CMAKE_BINARY_DIR}
-                       COMMAND yarpros ARGS type ${_type}
+                       COMMAND yarpidl_rosmsg ARGS ${_type}.msg
                        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                        DEPENDS ${_type}.msg)
     set_property(SOURCE ${CMAKE_BINARY_DIR}/${_type}.h PROPERTY GENERATED TRUE)
